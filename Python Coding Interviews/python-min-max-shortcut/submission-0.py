e9d9d78d367a529a380ba7499feb4aeca280ed89
JDIEvan <110309927+Evan287@users.noreply.github.com>
2026-08-02 14:59:10 -0700

    Add: python-min-max-shortcut - submission-0

---
+++ Python Coding Interviews/python-min-max-shortcut/submission-0.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+def disallow_negatives(num: int) -> int:
+    if max(0,num):
+        return num
+    else: 
+        return 0
+
+
+def max_difference(nums: List[int]) -> int:
+    #two pointer
+    #keep track of largest difference
+    num1, num2, greatestDiff = 0,0,0
+    for i in range(0,len(nums) -1):
+        num1 = nums[i]
+        num2 = nums[i+1]
+        if num2 - num1 > greatestDiff:
+            greatestDiff = num2 - num1
+    return greatestDiff
+
+
+
+
+# do not modify below this line
+print(disallow_negatives(-2))
+print(disallow_negatives(-1))
+print(disallow_negatives(0))
+print(disallow_negatives(1))
+print(disallow_negatives(2))
+
+print(max_difference([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(max_difference([1, 2, 3, 4, 5, 6, 8, 9]))
+print(max_difference([10, 1, 3, 7]))
+print(max_difference([2, 4, 7, 5, 7, 8, 4, 2]))
